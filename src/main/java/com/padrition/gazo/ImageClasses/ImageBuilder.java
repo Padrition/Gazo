0c6bc02ab7dda8a077480ff7c13a658eb17555bd
Padrition <dimastail23@gmail.com>
2020-05-04 23:01:27 +0600

    Added methods for generating image with certain number of random colors and for generating certain colors

--- src/main/java/com/padrition/gazo/ImageClasses/ImageBuilder.java
+++ src/main/java/com/padrition/gazo/ImageClasses/ImageBuilder.java
@@ -14,29 +14,121 @@ public class ImageBuilder {
     private static final int width = 200;
     private static final int height = 200;
 
-    BufferedImage bufferedImage = new BufferedImage(width , height , BufferedImage.TYPE_INT_RGB);
-
-    Graphics2D g2d = bufferedImage.createGraphics();
-
     public ImageBuilder(){}
-
+    
+    //set later to return the File
     public void generateImageWithRandomColors()throws IOException{
+        BufferedImage bufferedImage = new BufferedImage(width , height , BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = bufferedImage.createGraphics();
+        
         g2d.setColor(Color.WHITE);
         g2d.fillRect(0, 0, width, height);
         for(int y = 0; y <= height; y += 5){
-            int c = 0;
             for(int x = 0; x <=width; x += 5){
                 g2d.setColor(getRandomColor());
                 g2d.fillRect(x, y, x + 5, y + 5);
             }
-            c += 10;
         }
         g2d.dispose();
 
-        File file = new File("myimage.png");
+        File file = new File("FullyRandom.png");
         ImageIO.write(bufferedImage, "png" , file);
     }
     
+    public void generateImageWithRandomColors(int sizeOfBox)throws IOException{
+        BufferedImage bufferedImage = new BufferedImage(width , height , BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = bufferedImage.createGraphics();
+        
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(0, 0, width, height);
+        for(int y = 0; y <= height; y += sizeOfBox){
+            for(int x = 0; x <=width; x += sizeOfBox){
+                g2d.setColor(getRandomColor());
+                g2d.fillRect(x, y, x + sizeOfBox, y + sizeOfBox);
+            }
+        }
+        g2d.dispose();
+
+        File file = new File("FullyRandom.png");
+        ImageIO.write(bufferedImage, "png" , file);
+    }
+
+    public void generateImageWithNumberOfColors(int numberOfColors)throws IOException{
+        BufferedImage bufferedImage = new BufferedImage(width , height , BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = bufferedImage.createGraphics();
+
+        Color[] colors = new Color[numberOfColors];
+        for(int i = 0; i <= colors.length-1 ; i++){
+            colors[i] = getRandomColor();
+        }
+        for(int y = 0; y <= height; y += 5){
+            for(int x = 0; x <=width; x += 5){
+                int i = (int)(Math.random()*colors.length);
+                g2d.setColor(colors[i]);
+                g2d.fillRect(x, y, x + 5, y + 5);
+            }
+        }
+        g2d.dispose();
+
+        File file = new File("WithSomeColors.png");
+        ImageIO.write(bufferedImage, "png" , file);
+    }
+
+    public void generateImageWithNumberOfColors(int numberOfColors , int sizeOfBox)throws IOException{
+        BufferedImage bufferedImage = new BufferedImage(width , height , BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = bufferedImage.createGraphics();
+
+        Color[] colors = new Color[numberOfColors];
+        for(int i = 0; i <= colors.length-1 ; i++){
+            colors[i] = getRandomColor();
+        }
+        for(int y = 0; y <= height; y += sizeOfBox){
+            for(int x = 0; x <=width; x += sizeOfBox){
+                int i = (int)(Math.random()*colors.length);
+                g2d.setColor(colors[i]);
+                g2d.fillRect(x, y, x + sizeOfBox, y + sizeOfBox);
+            }
+        }
+        g2d.dispose();
+
+        File file = new File("WithSomeColors.png");
+        ImageIO.write(bufferedImage, "png" , file);
+    }
+
+    public void generateImageWithCertainColors(Color...colors)throws IOException{
+        BufferedImage bufferedImage = new BufferedImage(width , height , BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = bufferedImage.createGraphics();
+        
+        for(int y = 0; y <= height; y += 5){
+            for(int x = 0; x <=width; x += 5){
+                int i = (int)(Math.random()*colors.length);
+                g2d.setColor(colors[i]);
+                g2d.fillRect(x, y, x + 5, y + 5);
+            }
+        }
+        g2d.dispose();
+
+        File file = new File("WithCertainColors.png");
+        ImageIO.write(bufferedImage, "png" , file);
+    }
+
+    public void generateImageWithCertainColors(int sizeOfBox , Color...colors)throws IOException{
+        BufferedImage bufferedImage = new BufferedImage(width , height , BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = bufferedImage.createGraphics();
+        
+        for(int y = 0; y <= height; y += sizeOfBox){
+            for(int x = 0; x <=width; x += sizeOfBox){
+                int i = (int)(Math.random()*colors.length);
+                g2d.setColor(colors[i]);
+                g2d.fillRect(x, y, x + sizeOfBox, y + sizeOfBox);
+            }
+        }
+        g2d.dispose();
+
+        File file = new File("WithCertainColors.png");
+        ImageIO.write(bufferedImage, "png" , file);
+    }
+
     private Color getRandomColor(){
         int R = (int)(Math.random()*256);
         int G = (int)(Math.random()*256);
