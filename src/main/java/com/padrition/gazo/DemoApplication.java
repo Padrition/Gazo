977ba8e6f351a1760ef88b549e2e6a54a97d303f
Padrition <dimastail23@gmail.com>
2020-05-04 22:09:31 +0600

    ImageBuilder can generate an image with 5 by 5 boxes randomly colored

--- src/main/java/com/padrition/gazo/DemoApplication.java
+++ src/main/java/com/padrition/gazo/DemoApplication.java
@@ -1,13 +1,19 @@
 package com.padrition.gazo;
 
+import java.io.IOException;
+
+import com.padrition.gazo.ImageClasses.ImageBuilder;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		SpringApplication.run(DemoApplication.class, args);
+		ImageBuilder iBuilder = new ImageBuilder();
+		iBuilder.generateImageWithRandomColors();
 	}
 
 }
--- src/main/java/com/padrition/gazo/ImageClasses/ImageBuilder.java
+++ src/main/java/com/padrition/gazo/ImageClasses/ImageBuilder.java
@@ -20,21 +20,29 @@ public class ImageBuilder {
 
     public ImageBuilder(){}
 
-    public void createImage()throws IOException{
+    public void generateImageWithRandomColors()throws IOException{
         g2d.setColor(Color.WHITE);
         g2d.fillRect(0, 0, width, height);
-
-        g2d.setColor(Color.RED);
-        g2d.fillOval(0, 0, width, height);
-
+        for(int y = 0; y <= height; y += 5){
+            int c = 0;
+            for(int x = 0; x <=width; x += 5){
+                g2d.setColor(getRandomColor());
+                g2d.fillRect(x, y, x + 5, y + 5);
+            }
+            c += 10;
+        }
         g2d.dispose();
 
         File file = new File("myimage.png");
         ImageIO.write(bufferedImage, "png" , file);
-
-        file = new File("myimage2.jpg");
-        ImageIO.write(bufferedImage, "jpg", file);
     }
     
+    private Color getRandomColor(){
+        int R = (int)(Math.random()*256);
+        int G = (int)(Math.random()*256);
+        int B = (int)(Math.random()*256);
+        Color color = new Color(R , G , B);
+        return color;
+    }
 
 }
